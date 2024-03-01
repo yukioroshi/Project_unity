db2cc63dab5d0aa5fd07edc9a17a95451055de27
yukioroshi <148077899+yukioroshi@users.noreply.github.com>
2024-03-01 15:11:08 +0100

    add P2 and reset ball Pos

--- Assets/Score/goalPoints.cs
+++ Assets/Score/goalPoints.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class goalPoints : MonoBehaviour
 {
-    
+    Vector3 originalPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
     }
 
     // Update is called once per frame
@@ -22,12 +23,13 @@ public class goalPoints : MonoBehaviour
         if (collision.transform.name == "goalGr")
         {
             score.ScoreR += 1;
+            gameObject.transform.position = originalPos;
         }
 
         if (collision.transform.name == "goalGb")
         {
             score.ScoreB += 1;
-            Destroy(gameObject);
+            gameObject.transform.position = originalPos;
         }
     }
 
