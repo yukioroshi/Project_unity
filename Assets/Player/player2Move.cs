449aadf5f805b2543c55fb673a826d3313d38800
yukioroshi <148077899+yukioroshi@users.noreply.github.com>
2024-03-04 17:31:25 +0100

    player 2 moves

--- Assets/Player/player2Move.cs
+++ Assets/Player/player2Move.cs
@@ -6,8 +6,13 @@ public class player2Move : MonoBehaviour
 {
     private float horizontalInput;
     private float verticalInput;
+
     public float speed;
     public float jumpForce;
+
+    private float rotate;
+    public float rotationspeed;
+
     public bool isonground = true;
 
     private Rigidbody playerRb;
@@ -21,24 +26,21 @@ public class player2Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal2");
+        verticalInput = Input.GetAxis("Vertical2");
 
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
-        }
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
-        }
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
 
-        if (Input.GetKeyDown(KeyCode.RightShift) && isonground)
+        rotate = horizontalInput * rotationspeed * Time.deltaTime;
+        transform.Rotate(0f, rotate, 0f);
+
+
+        if (Input.GetKeyDown(KeyCode.RightAlt) && isonground)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isonground = false;
         }
+
     }
 
     private void OnCollisionEnter(Collision collision)
--- Assets/Player/playerMove.cs
+++ Assets/Player/playerMove.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class playermove : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
-    public bool isonground = true;
+
     private float horizontalInput;
     private float forwardInput;
+
+    private float rotate;
+    public float rotationspeed;
+
+    public bool isonground = true;
+
     private Rigidbody playerRb;
 
     // Start is called before the first frame update
@@ -24,26 +31,14 @@ public class playermove : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        //Get camera normalized directional vectors
-        Vector3 forward = GameObject.Find("PlayerCam").GetComponent<Camera>().transform.forward;
-        Vector3 right = GameObject.Find("PlayerCam").GetComponent<Camera>().transform.right;
-        forward.y = 0;
-        right.y = 0;
-        forward = forward.normalized;
-        right = right.normalized;
-
-        //create direction-relativ-input vectors
-        Vector3 forwardRelativeVerticalInput = forwardInput * speed * 0.01f * forward;
-        Vector3 rightRelativeHorizontalInput = horizontalInput * speed * 0.01f * right;
-
-        //create and apply camera relative movement
-        Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeHorizontalInput;
-        this.transform.Translate(cameraRelativeMovement, Space.World);
 
         //move the player forward
-        //transform.Translate(Vector3.forward * Time.deltaTime * forwardInput);
-        //transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
-        //Debug.Log(speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardInput * speed);
+
+        //rotate the player
+        rotate = horizontalInput * rotationspeed * Time.deltaTime;
+        transform.Rotate(0f, rotate, 0f);
+
 
         //let the player jump
         if (Input.GetKeyDown(KeyCode.Space) && isonground) 
