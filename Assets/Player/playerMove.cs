2c2cefee89264cde7d6a12d97d6730229bce2f97
yukioroshi <148077899+yukioroshi@users.noreply.github.com>
2024-03-06 23:32:43 +0100

    add sprint

--- Assets/Player/playerMove.cs
+++ Assets/Player/playerMove.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class playermove : MonoBehaviour
@@ -19,6 +20,12 @@ public class playermove : MonoBehaviour
     public bool isonground = true;
 
     private Rigidbody playerRb;
+
+    public float stamina = 50;
+    public float staminaConsumtion = 10;
+    public bool isrunning = false;
+    public float Cooldown = 5f;
+    private float currentCooldown = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -43,13 +50,37 @@ public class playermove : MonoBehaviour
        if (Input.GetKey(KeyCode.E))
         {
             playerRb.AddRelativeForce(Vector3.forward * speed, ForceMode.Acceleration);
-            
         }
         if (Input.GetKey(KeyCode.D))
         {
             playerRb.AddRelativeForce(Vector3.back * speed, ForceMode.Acceleration);
-
         }
+
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.E))
+        {
+            if(stamina >= 0 )
+            {
+                playerRb.AddRelativeForce(Vector3.forward * speed * 2, ForceMode.Acceleration);
+                stamina -= staminaConsumtion * Time.deltaTime;
+                isrunning = true;
+                Debug.Log(stamina);
+                
+            }
+            else
+            {
+                isrunning = false;
+            }
+        }
+        else
+        {
+            isrunning = false;
+        }
+
+        if (stamina <= 0)
+        {
+            stamina = 0;
+        }
+
 
         //let the player jump
         if (Input.GetKeyDown(KeyCode.Space) && isonground)
@@ -58,6 +89,7 @@ public class playermove : MonoBehaviour
             isonground = false;
         }
     }
+
    
     private void OnCollisionEnter(Collision collision)
     {
