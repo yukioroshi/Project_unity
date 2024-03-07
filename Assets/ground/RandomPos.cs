2f1ff14103324db92ffcede01f52838e78a9cac7
yukioroshi <148077899+yukioroshi@users.noreply.github.com>
2024-03-07 21:03:40 +0100

    placement aléatoire d'obstacle

---
+++ Assets/ground/RandomPos.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPos : MonoBehaviour
+{
+    public GameObject existingObject;
+
+    public GameObject objectToPlace;
+
+    public int numberOfObjectToPlace;
+
+    public float minX = -5f;
+    public float maxX = 5f;
+    public float minZ = -5f;
+    public float maxZ = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 existingObjectPosition = existingObject.transform.position;
+
+        for (int i = 0; i < numberOfObjectToPlace; i++)
+        {
+            float randomX = Random.Range(minX, maxX);
+            float randomZ = Random.Range(minZ, maxZ);
+
+            Vector3 newPostion = existingObjectPosition + new Vector3(randomX, 0F, randomZ);
+
+            Instantiate(objectToPlace, newPostion, Quaternion.Euler(45f,0f,0f));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
